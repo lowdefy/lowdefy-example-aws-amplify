3fc18bad126ca41aba905540e5d2d23f5978b3ac
Sam Tolmay <sam@lowdefy.com>
2023-03-06 13:52:07 +0200

    test

--- setBuildCommand.mjs
+++ setBuildCommand.mjs
@@ -10,6 +10,8 @@ packageJson.scripts.build = "next build";
 packageJson.scripts.dev = "next dev";
 packageJson.scripts.start = "next start";
 
+delete packageJson.packageManager;
+
 writeFileSync(
   "./package.json",
   JSON.stringify(packageJson, null, 2).concat("\n")
